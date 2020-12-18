3389c4a8b7950501fa74fbf0601de86b380435c0
Sandip Das <sandeepdas.0519@gmail.com>
2020-12-18 14:48:09 +0530

    Updates

---
+++ precache-manifest.f3d8a0fa5ebb8b292eb270c6427728f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212bf17e0c35d9d9197b75db5bd6ff13",
+    "url": "/widgets/index.html"
+  },
+  {
+    "revision": "112dbd68242cf1003791",
+    "url": "/widgets/static/js/2.85e7640b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/widgets/static/js/2.85e7640b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cdd2f4578bad6cff6dd",
+    "url": "/widgets/static/js/main.7f4cbda6.chunk.js"
+  },
+  {
+    "revision": "a3b7248df9ec66a8e8f5",
+    "url": "/widgets/static/js/runtime-main.4d12c3f6.js"
+  }
+]);
